00fe385d697d1d777cc11220995917e2afa02a56
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-06 17:13:08 -0500

    added login feature and corrected routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_secure_password #get algorythm from bcrypt to give us a secure
+  validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'sessions#new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'user#new'
+  post '/signup' => 'user#create'
+
+  get '/products' => 'products#index'
+
   resources :reviews
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
